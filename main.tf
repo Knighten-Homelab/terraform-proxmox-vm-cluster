8adbd5afad4d9c9f69b5cc0266afe4a0690a2764
Johnny K <johnny@knightenuniforms.com>
2024-10-25 03:11:50 +0000

    feat: introduced core vm metadata vars

--- main.tf
+++ main.tf
@@ -1,49 +1,68 @@
 terraform {
-  required_version = "1.6.6"
+  required_version = "1.9.8"
 }
 
 module "pve-vm-cluster" {
-  source = "github.com/Johnny-Knighten/terraform-homelab-pve-vm?ref=v1.0.0"
-  count  = var.cluster_node_count
+  source = "github.com/Johnny-Knighten/terraform-homelab-pve-vm?ref=1.3.4"
+  count  = var.node_count
 
-  // Authentication Vars
-  ansible_service_account_ssh_key = var.ansible_service_account_ssh_key
+  pve_node    = var.use_same_node ? var.pve_node : var.pve_node_assignments[count.index]
+  pve_vm_id   = var.auto_increment_vm_id ? (var.auto_increment_vm_id_start != null ? var.auto_increment_vm_id_start + count.index : 0) : var.vm_id_list[count.index]
+  pve_vm_name = var.auto_increment_vm_names ? format("%s-%s", var.vm_base_name, count.index) : var.vm_name_list[count.index]
+  pve_vm_desc = var.same_description ? var.vm_desc : var.vm_desc_list[count.index]
 
-  // PVE Vars Shared Across All VMs
-  pve_template              = var.pve_template
-  pve_vm_full_clone         = var.pve_vm_full_clone
-  pve_vm_core_count         = var.pve_vm_core_count
-  pve_vm_memory             = var.pve_vm_memory
-  pve_vm_gateway            = var.pve_vm_gateway
-  pve_vm_dns_server         = var.pve_vm_dns_server
-  pve_vm_vlan_tag           = var.pve_vm_vlan_tag
-  pve_vm_packet_queue_count = var.pve_vm_packet_queue_count
+  pdns_zone        = "homelab.lan"
+  pdns_record_name = format("%s-%s", "test-vm", count.index)
 
-  // PVE Vars Unique To Each VM
-  pve_vm_name                  = format("%s-%s", var.pve_vm_name_base, count.index)
-  pve_vm_desc                  = var.pve_vm_desc_list[count.index]
-  pve_node                     = var.pve_node_list[count.index]
-  pve_vm_use_static_ip         = var.pve_vm_use_static_ip_list[count.index]
-  pve_vm_ip                    = var.pve_vm_ip_list[count.index]
-  pve_vm_subnet_network_bits   = var.pve_vm_subnet_network_bits_list[count.index]
-  pve_vm_boot_on_start         = var.pve_vm_boot_on_start_list[count.index]
-  pve_vm_startup_options       = var.pve_vm_startup_options_list[count.index]
-  pve_vm_disk_storage_location = var.pve_vm_disk_storage_location_list[count.index]
-
-
-  // PowerDNS Vars Shared Across All VMs
-  pdns_zone = var.pdns_zone
-
-  // PowerDNS Vars Unique To Each VM
-  pdns_record_name = var.pdns_record_name_list[count.index]
-
-  // AWX Vars Shared Across All VMs
-  awx_organization = var.awx_organization
-  awx_inventory    = var.awx_inventory
-  awx_host_groups  = var.awx_host_groups
-
-  // AWX Vars Unique To Each VM
-  awx_host_name        = var.awx_host_name_list[count.index]
-  awx_host_description = var.awx_host_description_list[count.index]
-
+  awx_organization     = "Homelab"
+  awx_inventory        = "Homelab Endpoints"
+  awx_host_groups      = ["proxmox-hosts"]
+  awx_host_name        = format("%s-%s", "test-vm", count.index)
+  awx_host_description = "A test VM created by Terraform"
 }
+
+# module "pve-vm-cluster" {
+#   source = "github.com/Johnny-Knighten/terraform-homelab-pve-vm?ref=1.3.4"
+#   count  = var.cluster_node_count
+
+#   // Authentication Vars
+#   ansible_service_account_ssh_key = var.ansible_service_account_ssh_key
+
+#   // PVE Vars Shared Across All VMs
+#   pve_template              = var.pve_template
+#   pve_vm_full_clone         = var.pve_vm_full_clone
+#   pve_vm_core_count         = var.pve_vm_core_count
+#   pve_vm_memory             = var.pve_vm_memory
+#   pve_vm_gateway            = var.pve_vm_gateway
+#   pve_vm_dns_server         = var.pve_vm_dns_server
+#   pve_vm_vlan_tag           = var.pve_vm_vlan_tag
+#   pve_vm_packet_queue_count = var.pve_vm_packet_queue_count
+
+#   // PVE Vars Unique To Each VM
+#   pve_vm_name                  = format("%s-%s", var.pve_vm_name_base, count.index)
+#   pve_vm_desc                  = var.pve_vm_desc_list[count.index]
+#   pve_node                     = var.pve_node_list[count.index]
+#   pve_vm_use_static_ip         = var.pve_vm_use_static_ip_list[count.index]
+#   pve_vm_ip                    = var.pve_vm_ip_list[count.index]
+#   pve_vm_subnet_network_bits   = var.pve_vm_subnet_network_bits_list[count.index]
+#   pve_vm_boot_on_start         = var.pve_vm_boot_on_start_list[count.index]
+#   pve_vm_startup_options       = var.pve_vm_startup_options_list[count.index]
+#   pve_vm_disk_storage_location = var.pve_vm_disk_storage_location_list[count.index]
+
+
+#   // PowerDNS Vars Shared Across All VMs
+#   pdns_zone = var.pdns_zone
+
+#   // PowerDNS Vars Unique To Each VM
+#   pdns_record_name = var.pdns_record_name_list[count.index]
+
+#   // AWX Vars Shared Across All VMs
+#   awx_organization = var.awx_organization
+#   awx_inventory    = var.awx_inventory
+#   awx_host_groups  = var.awx_host_groups
+
+#   // AWX Vars Unique To Each VM
+#   awx_host_name        = var.awx_host_name_list[count.index]
+#   awx_host_description = var.awx_host_description_list[count.index]
+
+# }
--- vars.tf
+++ vars.tf
@@ -1,150 +1,235 @@
-#################
-# Cluster Nodes #
-#################
+#####################
+# Cluster Variables #
+#####################
 
-variable "cluster_node_count" {
+variable "node_count" {
   type        = number
   description = "number of vm nodes to create"
-  default     = 3
-}
-
-############################
-#  Required Auth Variables #
-############################
-
-variable "ansible_service_account_ssh_key" {
-  type        = string
-  description = "SSH key for the ansible service account"
+  default     = 1
 }
 
 #################
 # PVE Variables #
 #################
 
-variable "pve_vm_name_base" {
-  description = "base name of the VM to create"
-  type        = string
-}
+# VM ID Variables
 
-variable "pve_template" {
-  description = "name of the PVE template to clone"
-  type        = string
-}
-
-variable "pve_vm_full_clone" {
-  description = "whether or not to do a full clone"
+variable "auto_increment_vm_id" {
   type        = bool
+  description = "whether or not to auto increment the VM ID"
+  default     = true
 }
 
-variable "pve_vm_core_count" {
-  description = "number of cores to allocate to the VM"
+variable "auto_increment_vm_id_start" {
   type        = number
+  description = "starting number to use for auto incrementing VM IDs, otherwise uses next available ID"
+  default     = null
 }
 
-variable "pve_vm_memory" {
-  description = "amount of memory to allocate to the VM in MB"
-  type        = number
+variable "vm_id_list" {
+  type        = list(number)
+  description = "list of VM IDs for each VM"
 }
 
-variable "pve_vm_gateway" {
-  description = "gateway to use for the VM, must be set if using static IP"
+# VM Name Variables
+
+variable "auto_increment_vm_names" {
+  type        = bool
+  description = "whether or not to auto increment node names (node-0, node-1, etc)"
+  default     = true
+}
+
+variable "vm_base_name" {
   type        = string
+  description = "base name to use for auto generated node names"
+  default     = "node"
 }
 
-variable "pve_vm_dns_server" {
-  description = "ip of vm's dns server"
-  type        = string
-}
-
-variable "pve_vm_vlan_tag" {
-  description = "VLAN tag to use for the VM"
-  type        = number
-}
-
-variable "pve_vm_packet_queue_count" {
-  description = "number of VM packet queues"
-  type        = number
-}
-
-variable "pve_vm_desc_list" {
-  description = "list of descriptions for each VM"
+variable "vm_name_list" {
   type        = list(string)
+  description = "list of names for each VM"
+  default     = []
 }
 
-variable "pve_node_list" {
+# VM Node Assignment Variables
+
+variable "use_same_node" {
+  type        = bool
+  description = "whether or not to assign all VMs to the same PVE node"
+  default     = true
+}
+
+variable "pve_node" {
+  type        = string
+  description = "name of the ProxMox node to create all VMs on"
+  default     = "primary"
+}
+
+variable "pve_node_assignments" {
+  type        = list(string)
   description = "list of nodes to deploy for each VM"
-  type        = list(string)
+  default     = []
 }
 
-variable "pve_vm_use_static_ip_list" {
-  description = "list of booleans indicating if each VM should use a static IP"
-  type        = list(bool)
+#  VM Description Variables
+
+variable "same_description" {
+  type        = bool
+  description = "whether or not to use a unique description for each VM"
+  default     = true
 }
 
-variable "pve_vm_ip_list" {
-  description = "list of IP addresses for each VM"
-  type        = list(string)
-}
-
-variable "pve_vm_subnet_network_bits_list" {
-  description = "list of subnet network bits for each VM"
-  type        = list(string)
-}
-
-variable "pve_vm_boot_on_start_list" {
-  description = "list of booleans indicating if each VM should boot on start"
-  type        = list(bool)
-}
-
-variable "pve_vm_startup_options_list" {
-  description = "list of startup options for each VM"
-  type        = list(string)
-}
-
-variable "pve_vm_disk_storage_location_list" {
-  description = "list of disk storage locations for each VM"
-  type        = list(string)
-}
-
-############
-# PowerDNS #
-############
-
-variable "pdns_zone" {
-  description = "name of the PowerDNS zone to create the record in"
+variable "vm_desc" {
   type        = string
+  description = "description to use for all VMs"
+  default     = "cluster node"
 }
 
-variable "pdns_record_name_list" {
-  description = "list of record names for each VM"
+variable "vm_desc_list" {
   type        = list(string)
+  description = "list of descriptions for each VM"
+  default     = []
 }
 
-#################
-# AWX Variables #
-#################
 
-variable "awx_organization" {
-  description = "name of the AWX organization to create the host in"
-  type        = string
-}
 
-variable "awx_inventory" {
-  description = "name of the AWX inventory to create the host in"
-  type        = string
-}
+# ############################
+# #  Required Auth Variables #
+# ############################
 
-variable "awx_host_groups" {
-  description = "comma separated list of AWX host groups to add the host tos"
-  type        = list(string)
-}
+# variable "ansible_service_account_ssh_key" {
+#   type        = string
+#   description = "SSH key for the ansible service account"
+# }
 
-variable "awx_host_name_list" {
-  description = "list of names for AWX hosts for each VM"
-  type        = list(string)
-}
+# #################
+# # PVE Variables #
+# #################
 
-variable "awx_host_description_list" {
-  description = "list of descriptions for AWX host for each VM"
-  type        = list(string)
-}
+# variable "pve_vm_name_base" {
+#   description = "base name of the VM to create"
+#   type        = string
+# }
+
+# variable "pve_template" {
+#   description = "name of the PVE template to clone"
+#   type        = string
+# }
+
+# variable "pve_vm_full_clone" {
+#   description = "whether or not to do a full clone"
+#   type        = bool
+# }
+
+# variable "pve_vm_core_count" {
+#   description = "number of cores to allocate to the VM"
+#   type        = number
+# }
+
+# variable "pve_vm_memory" {
+#   description = "amount of memory to allocate to the VM in MB"
+#   type        = number
+# }
+
+# variable "pve_vm_gateway" {
+#   description = "gateway to use for the VM, must be set if using static IP"
+#   type        = string
+# }
+
+# variable "pve_vm_dns_server" {
+#   description = "ip of vm's dns server"
+#   type        = string
+# }
+
+# variable "pve_vm_vlan_tag" {
+#   description = "VLAN tag to use for the VM"
+#   type        = number
+# }
+
+# variable "pve_vm_packet_queue_count" {
+#   description = "number of VM packet queues"
+#   type        = number
+# }
+
+# variable "pve_vm_desc_list" {
+#   description = "list of descriptions for each VM"
+#   type        = list(string)
+# }
+
+# variable "pve_node_list" {
+#   description = "list of nodes to deploy for each VM"
+#   type        = list(string)
+# }
+
+# variable "pve_vm_use_static_ip_list" {
+#   description = "list of booleans indicating if each VM should use a static IP"
+#   type        = list(bool)
+# }
+
+# variable "pve_vm_ip_list" {
+#   description = "list of IP addresses for each VM"
+#   type        = list(string)
+# }
+
+# variable "pve_vm_subnet_network_bits_list" {
+#   description = "list of subnet network bits for each VM"
+#   type        = list(string)
+# }
+
+# variable "pve_vm_boot_on_start_list" {
+#   description = "list of booleans indicating if each VM should boot on start"
+#   type        = list(bool)
+# }
+
+# variable "pve_vm_startup_options_list" {
+#   description = "list of startup options for each VM"
+#   type        = list(string)
+# }
+
+# variable "pve_vm_disk_storage_location_list" {
+#   description = "list of disk storage locations for each VM"
+#   type        = list(string)
+# }
+
+# ############
+# # PowerDNS #
+# ############
+
+# variable "pdns_zone" {
+#   description = "name of the PowerDNS zone to create the record in"
+#   type        = string
+# }
+
+# variable "pdns_record_name_list" {
+#   description = "list of record names for each VM"
+#   type        = list(string)
+# }
+
+# #################
+# # AWX Variables #
+# #################
+
+# variable "awx_organization" {
+#   description = "name of the AWX organization to create the host in"
+#   type        = string
+# }
+
+# variable "awx_inventory" {
+#   description = "name of the AWX inventory to create the host in"
+#   type        = string
+# }
+
+# variable "awx_host_groups" {
+#   description = "comma separated list of AWX host groups to add the host tos"
+#   type        = list(string)
+# }
+
+# variable "awx_host_name_list" {
+#   description = "list of names for AWX hosts for each VM"
+#   type        = list(string)
+# }
+
+# variable "awx_host_description_list" {
+#   description = "list of descriptions for AWX host for each VM"
+#   type        = list(string)
+# }
