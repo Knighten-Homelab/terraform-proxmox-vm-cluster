442ca87d25be401d95081cee4d39ffa27841a40a
Johnny K <johnny@knightenuniforms.com>
2024-10-25 04:04:39 +0000

    feat: added the remaining pve variables to module

--- vars.tf
+++ vars.tf
@@ -92,145 +92,188 @@ variable "vm_desc_list" {
   default     = []
 }
 
+# Clone/Template Variables 
 
+variable "pve_is_clone" {
+  type        = bool
+  description = "Flag to determine if the VM is a clone or not (based off iso)"
+  default     = true
+}
 
-# ############################
-# #  Required Auth Variables #
-# ############################
+variable "pve_template" {
+  type        = string
+  description = "name of the PVE template to clone"
+  default     = "ubuntu-server-22-04-base-template-homelab"
+}
 
-# variable "ansible_service_account_ssh_key" {
-#   type        = string
-#   description = "SSH key for the ansible service account"
-# }
+variable "pve_vm_full_clone" {
+  type        = bool
+  description = "whether or not to do a full clone of the template"
+  default     = true
+}
 
-# #################
-# # PVE Variables #
-# #################
+variable "pve_vm_iso" {
+  type        = string
+  description = "iso to use for the VM"
+  default     = ""
+}
 
-# variable "pve_vm_name_base" {
-#   description = "base name of the VM to create"
-#   type        = string
-# }
+# Boot Options
 
-# variable "pve_template" {
-#   description = "name of the PVE template to clone"
-#   type        = string
-# }
+variable "pve_vm_boot_on_start" {
+  type        = bool
+  description = "whether or not to boot the VM on start"
+  default     = false
+}
 
-# variable "pve_vm_full_clone" {
-#   description = "whether or not to do a full clone"
-#   type        = bool
-# }
+variable "pve_vm_startup_options" {
+  type        = string
+  description = "startup options seperated via comma: boot order (order=), startup delay(up=), and shutdown delay(down=)"
+  default     = ""
+}
 
-# variable "pve_vm_core_count" {
-#   description = "number of cores to allocate to the VM"
-#   type        = number
-# }
+variable "pve_vm_boot_disk" {
+  type        = string
+  description = "boot disk for the VM"
+  default     = null
+}
 
-# variable "pve_vm_memory" {
-#   description = "amount of memory to allocate to the VM in MB"
-#   type        = number
-# }
+# CPU Options
 
-# variable "pve_vm_gateway" {
-#   description = "gateway to use for the VM, must be set if using static IP"
-#   type        = string
-# }
+variable "pve_vm_core_count" {
+  type        = string
+  description = "number of cores to allocate to the VM"
+  default     = "2"
+}
 
-# variable "pve_vm_dns_server" {
-#   description = "ip of vm's dns server"
-#   type        = string
-# }
+variable "pve_vm_cpu_type" {
+  type        = string
+  description = "type of CPU to use for the VM"
+  default     = "host"
+}
 
-# variable "pve_vm_vlan_tag" {
-#   description = "VLAN tag to use for the VM"
-#   type        = number
-# }
+variable "pve_vm_sockets" {
+  type        = string
+  description = "number of sockets to allocate to the VM"
+  default     = "1"
+}
 
-# variable "pve_vm_packet_queue_count" {
-#   description = "number of VM packet queues"
-#   type        = number
-# }
+# Memory Options
 
-# variable "pve_vm_desc_list" {
-#   description = "list of descriptions for each VM"
-#   type        = list(string)
-# }
+variable "pve_vm_memory_size" {
+  type        = number
+  description = "amount of memory to allocate to the VM in MB"
+  default     = 2048
+}
 
-# variable "pve_node_list" {
-#   description = "list of nodes to deploy for each VM"
-#   type        = list(string)
-# }
+variable "pve_memory_balloon" {
+  type        = number
+  description = "whether or not to use memory ballooning"
+  default     = 0
+}
 
-# variable "pve_vm_use_static_ip_list" {
-#   description = "list of booleans indicating if each VM should use a static IP"
-#   type        = list(bool)
-# }
+# Network Options
 
-# variable "pve_vm_ip_list" {
-#   description = "list of IP addresses for each VM"
-#   type        = list(string)
-# }
+variable "pve_vm_networks" {
+  type = list(object({
+    model  = string
+    bridge = string
+    tag    = optional(string)
+    queues = optional(string)
+  }))
+  description = "List of network configurations for the VM"
+  default = [
+    {
+      model  = "virtio"
+      bridge = "vmbr0"
+      tag    = "-1"
+      queues = "1"
+    }
+  ]
+}
 
-# variable "pve_vm_subnet_network_bits_list" {
-#   description = "list of subnet network bits for each VM"
-#   type        = list(string)
-# }
+# Cloud-Init Options
 
-# variable "pve_vm_boot_on_start_list" {
-#   description = "list of booleans indicating if each VM should boot on start"
-#   type        = list(bool)
-# }
+variable "pve_use_cloud_init" {
+  type        = bool
+  description = "whether or not to use the cloud_init"
+  default     = true
+}
 
-# variable "pve_vm_startup_options_list" {
-#   description = "list of startup options for each VM"
-#   type        = list(string)
-# }
+variable "pve_ssh_user" {
+  type        = string
+  description = "ssh user to used to provision the VM"
+  default     = "ansible"
+}
 
-# variable "pve_vm_disk_storage_location_list" {
-#   description = "list of disk storage locations for each VM"
-#   type        = list(string)
-# }
+variable "pve_ssh_private_key" {
+  type        = string
+  description = "ssh private key to used to provision the VM"
+  default     = ""
+}
 
-# ############
-# # PowerDNS #
-# ############
+variable "all_dhcp" {
+  type        = bool
+  description = "whether or not to use DHCP for all VMs"
+  default     = true
+}
 
-# variable "pdns_zone" {
-#   description = "name of the PowerDNS zone to create the record in"
-#   type        = string
-# }
+variable "static_addresses" {
+  type = list(object({
+    ip_address   = string
+    network_bits = string
+    gateway      = string
+  }))
+}
 
-# variable "pdns_record_name_list" {
-#   description = "list of record names for each VM"
-#   type        = list(string)
-# }
+variable "pve_vm_dns_server" {
+  type        = string
+  description = "ip of vm's dns server"
+  default     = ""
+}
 
-# #################
-# # AWX Variables #
-# #################
+variable "pve_cloudinit_storage_location" {
+  type        = string
+  description = "storage location for the cloud-init iso"
+  default     = "local-zfs"
+}
 
-# variable "awx_organization" {
-#   description = "name of the AWX organization to create the host in"
-#   type        = string
-# }
+# Disk Configuration
 
-# variable "awx_inventory" {
-#   description = "name of the AWX inventory to create the host in"
-#   type        = string
-# }
+variable "pve_vm_disk_size" {
+  type        = string
+  description = "size of the VM disk "
+  default     = "20G"
+}
 
-# variable "awx_host_groups" {
-#   description = "comma separated list of AWX host groups to add the host tos"
-#   type        = list(string)
-# }
+variable "same_disk_storage_location" {
+  type        = bool
+  description = "whether or not to use the same storage location for all VM disks"
+  default     = true
+}
 
-# variable "awx_host_name_list" {
-#   description = "list of names for AWX hosts for each VM"
-#   type        = list(string)
-# }
+variable "pve_vm_disk_storage_location" {
+  type        = string
+  description = "storage location for the VM disk"
+  default     = "local-zfs"
+}
 
-# variable "awx_host_description_list" {
-#   description = "list of descriptions for AWX host for each VM"
-#   type        = list(string)
-# }
+variable "pve_vm_disk_storage_location_list" {
+  type        = list(string)
+  description = "list of storage locations for each VM disk"
+  default     = []
+}
+
+variable "pve_vm_scsihw" {
+  type        = string
+  description = "scsi hardware to use for the VM"
+  default     = "virtio-scsi-pci"
+}
+
+# Agent Options
+
+variable "pve_vm_agent" {
+  type        = number
+  description = "whether or not to use the agent"
+  default     = 1
+}
