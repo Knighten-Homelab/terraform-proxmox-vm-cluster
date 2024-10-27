9320bd68eeccd26a9be880120c6398fb345bdbb7
Johnny K <johnny@knightenuniforms.com>
2024-10-27 06:47:55 +0000

    fix: correct accidentl bad name for vm_dns_records output

--- output.tf
+++ output.tf
@@ -4,7 +4,7 @@ output "vm_details" {
       vm_id          = module.pve-vm-cluster[i].vm_id
       vm_name        = module.pve-vm-cluster[i].vm_name
       vm_ip_address  = module.pve-vm-cluster[i].vm_ip_address
-      vm_dns_records = module.pve-vm-cluster[i].dns-vm_dns_records
+      vm_dns_records = module.pve-vm-cluster[i].vm_dns_records
     }
   ]
   description = "List of VM details including VM ID, VM Descrip,SIP, and DNS record."
