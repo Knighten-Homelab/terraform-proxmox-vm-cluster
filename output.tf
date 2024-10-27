d7143d36d3fd1a5a7af7641af08d4aa7e23e78f5
Johnny K <johnny@knightenuniforms.com>
2024-10-27 06:45:51 +0000

    fix: corrected vars used in output

--- output.tf
+++ output.tf
@@ -1,9 +1,11 @@
 output "vm_details" {
   value = [
     for i in range(var.node_count) : {
-      ip-address = module.pve-vm-cluster[i].ip-address
-      dns-record = module.pve-vm-cluster[i].dns-record
+      vm_id          = module.pve-vm-cluster[i].vm_id
+      vm_name        = module.pve-vm-cluster[i].vm_name
+      vm_ip_address  = module.pve-vm-cluster[i].vm_ip_address
+      vm_dns_records = module.pve-vm-cluster[i].dns-vm_dns_records
     }
   ]
-  description = "List of VM details including IP and DNS record."
+  description = "List of VM details including VM ID, VM Descrip,SIP, and DNS record."
 }
